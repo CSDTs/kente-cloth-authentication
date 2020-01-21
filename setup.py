f0e20940026e52915b866a3a26efe11a48549a79
Kwame Porter Robinson <kporterrobinson@gmail.com>
2020-01-21 12:04:19 -0500

    (fix) adds cookie cutter directory layout

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Culturally situated tools project for kente cloth authentication',
+    author='Culturally Situated Teools, Dr Eglash',
+    license='BSD-3',
+)
