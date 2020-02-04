4941d90521655c807ed4ca263eb2c285c6bcadb0
Andrew Hunn <ahunn59@gmail.com>
2020-02-04 16:53:09 -0500

    Feat: Random image sub sections (#5)
    * feat: add random image sub section generation

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,17 +3,30 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-
+from process_image import generate_subsections
 
 @click.command()
+
+
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.option('--seed', prompt=True, type=int)
+@click.option('--n', prompt=True, type=int)
+@click.option('--width', prompt=True, type=int)
+@click.option('--height', prompt=True, type=int)
+@click.option('--xrotation', prompt=True, type=int)
+@click.option('--yrotation', prompt=True, type=int)
+@click.option('--zrotation', prompt=True, type=int)
+
+def main(input_filepath, output_filepath, seed, n, width, height, xrotation, yrotation, zrotation):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    generate_subsections(seed, n, width, height, input_filepath, output_filepath, (xrotation, yrotation, zrotation))
+    
+
 
 
 if __name__ == '__main__':
